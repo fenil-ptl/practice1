707da583f03ea9bbb203a41942436eb00abaeab1
fenil patel <126600325+fenil-ptl@users.noreply.github.com>
2024-02-01 23:08:39 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    printf(" Hello coders ");
+    printf(" Fenil bhai ");
+    printf(" Delete ");
+      
+}
+void fenil()
+{
+    printf(" Fenu bhai ");
+}
